c6a4994373469b3120329fba8e29bea49c092909
santy1493 <santy1493@gmail.com>
2021-09-30 14:19:54 -0300

    v2.00

---
+++ TP-02/Entidades/Suv.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades
+{
+    public class Suv : Vehiculo
+    {
+        public Suv(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+        }
+        /// <summary>
+        /// SUV son 'Grande'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SUV");
+
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
